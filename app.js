656c917d117bcaeec3749cc0b9b38f9e6ec32ea5
lovro12 <lovro.katic@fer.hr>
2021-12-13 07:15:05 +0100

    fourth commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
